c9a1f4093922001356a6243bc71e00b83c3498de
Bijas48 <adriankwk9m@gmail.com>
2024-06-03 22:24:46 +0700

    Merge The Auth

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const sessionMiddleware = require("./src/middlewares/session");
+const routesthisapp = require("./src/routes/routes");
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(sessionMiddleware);
 
 // Define Routes
-app.use('/api/transactions', require('./src/routes/transactionRoutes'));
+app.use("/api/transactions", require("./src/routes/transactionRoutes"));
+app.use("/", routesthisapp);
 
-const PORT = process.env.PORT;
+// Handle 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, that route doesn't exist.");
+});
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(port, () => {
+  console.log(`FinancyQ listening on "http://localhost:${port}"`);
+});
